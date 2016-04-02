e4c1e8bc56b85cc2ebfb2735f5c18b6bf553f608
stone-SJH <wy30123@163.com>
2016-04-02 23:42:43 +0800

    inherit example

---
+++ inheritanceTest.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+//#define INHERIT
+class A
+{
+private:
+	int privatedateA;
+protected:
+	int protecteddateA;
+public:
+	int publicdateA;
+};
+class B :public A
+{
+public:
+	void funct()
+	{
+		int b;
+		//b = privatedateA;   //error：基类中私有成员在派生类中是不可见的
+		b = protecteddateA; //ok：基类的保护成员在派生类中为保护成员
+		b = publicdateA;    //ok：基类的公共成员在派生类中为公共成员
+	}
+};
+class C :private A
+{
+public:
+	void funct()
+	{
+		int c;
+		//c = privatedateA;    //error：基类中私有成员在派生类中是不可见的
+		c = protecteddateA;  //ok：基类的保护成员在派生类中为私有成员
+		c = publicdateA;     //ok：基类的公共成员在派生类中为私有成员
+	}
+};
+class D :protected A
+{
+public:
+	void funct()
+	{
+		int d;
+		//d = privatedateA;   //error：基类中私有成员在派生类中是不可见的
+		d = protecteddateA; //ok：基类的保护成员在派生类中为保护成员
+		d = publicdateA;    //ok：基类的公共成员在派生类中为保护成员
+	}
+};
+#ifdef INHERIT
+int main()
+{
+	A* test1 = new B();
+	//A* test2 = new C(); //error
+	//A* test3 = new D(); //error
+	
+	int a;
+
+	A* objB = new B();
+	//a = objB->privatedateA;   //error：基类中私有成员在派生类中是不可见的,对对象不可见
+	//a = objB->protecteddateA; //error：基类的保护成员在派生类中为保护成员，对对象不可见
+	a = objB->publicdateA;    //ok：基类的公共成员在派生类中为公共成员，对对象可见
+
+	C objC;
+	//a = objC.privatedateA;   //error：基类中私有成员在派生类中是不可见的,对对象不可见
+	//a = objC.protecteddateA; //error：基类的保护成员在派生类中为私有成员，对对象不可见
+	//a = objC.publicdateA;    //error：基类的公共成员在派生类中为私有成员，对对象不可见
+
+	D objD;
+	//a = objD.privatedateA;   //error：基类中私有成员在派生类中是不可见的,对对象不可见
+	//a = objD.protecteddateA; //error：基类的保护成员在派生类中为保护成员，对对象不可见
+	//a = objD.publicdateA;    //error：基类的公共成员在派生类中为保护成员，对对象不可见
+
+}
+#endif
